164f6bbe7ed83a01cca28c8f9829da9512884b0e
ianqii <conradie.ian2@gmail.com>
2024-09-29 21:40:03 +0200

    structuring and email

--- send_email.php
+++ send_email.php
@@ -1,4 +1,8 @@
 <?php
+// Enable error reporting for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Get the raw POST data
 $data = json_decode(file_get_contents('php://input'), true);
 
@@ -7,16 +11,19 @@ if ($data) {
     $email = htmlspecialchars($data['email']);
     $message = htmlspecialchars($data['message']);
 
+    // Validate email format
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "Invalid email format.";
         exit;
     }
 
+    // Set your email address
     $to = "conradie.ian2@gmail.com";  
     $subject = "New Message from Portfolio Contact Form";
     $body = "Name: $name\nEmail: $email\n\nMessage:\n$message\n";
     $headers = "From: $email\r\nReply-To: $email\r\n";
 
+    // Send the email
     if (mail($to, $subject, $body, $headers)) {
         echo "Message sent successfully!";
     } else {
